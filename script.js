056e40a8176488e27f59debeda1c4f33a649761f
Carl Wiede <carlwie@student.chalmers.se>
2023-01-19 16:43:40 +0100

    Added usage of LocalStorage to keep DarkMode setting through reloads

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 window.onload = function() {   
 
-    // Dark mode toggle
+    // Pointers to section and toggle elements
     const sec = document.querySelector('.sec');
     const toggle = document.querySelector('.toggle');
+    
+    // Dark mode toggler
     toggle.addEventListener("click", function (){
         sec.classList.toggle('dark');
+        if (sec.classList.contains('dark')) {
+            localStorage.setItem('darkMode', 'on');
+        } else {
+            localStorage.setItem('darkMode', 'off');
+        }
     });
+
+    // Set initial Dark/Light
+    isDark = localStorage.getItem('darkMode');
+    if (isDark === null) {
+        // First visist on site, let there be darkness
+        localStorage.setItem('darkMode', 'on');
+    } else if (isDark === 'off') {
+        sec.classList.toggle('dark');
+    }
 
 }
